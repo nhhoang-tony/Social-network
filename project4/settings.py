3bd01c821f821f4b59409b9c09010f3db3cc7a75
Tony Nguyen <t.nguyen@vbreathe.com>
2024-01-15 09:07:46 +1000

    Change sqlite to postgresql

--- project4/settings.py
+++ project4/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'project4.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'Db2c1*Ggf41BG45-112AFC5fBB5CbAAg',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '18207',
     }
 }
 
